6e05fc80fbd5e8e048cd3bb7b7d50b1a1ed3a4d0
s13g <ayflix0@gmail.com>
2022-08-07 19:39:11 +0100

    js page

--- styles/app.js
+++ styles/app.js
@@ -1,12 +1,28 @@
-const signIn = document.querySelector("#sign-in");
+const signIn = document.querySelector(".sign-in");
 const close = document.querySelector(".x");
-const entireCover = document.querySelector(".entire-cover");
+const entireCover = document.querySelector(".entire-cover-login");
 const overlay = document.querySelector(".overlay");
 
-const toggle = function() {
+const toggle = function () {
     entireCover.classList.toggle("show")
     overlay.classList.toggle("show")
+
+    // when modal is shown, page body is fixed
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${window.scrollY}px`;
+
+};
+
+signIn.addEventListener("click", toggle);
+close.addEventListener("click", toggle);
+
+
+const scroll = function () {
+    entireCover.classList.toggle("close")
+    // when modal is hidden, page body starts to scroll
+    const scrollY = document.body.style.top;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
 }
 
-signIn.addEventListener('click', toggle);
-close.addEventListener('click', toggle);
