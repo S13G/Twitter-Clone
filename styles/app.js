45e90d25a4c62bdbecdd0cb6fe523a9544be95a1
s13g <ayflix0@gmail.com>
2022-08-07 22:48:16 +0100

    hover kay

--- styles/app.js
+++ styles/app.js
@@ -4,34 +4,35 @@ const entireCover = document.querySelector(".entire-cover-login");
 const overlay = document.querySelector(".overlay");
 
 const toggle = function () {
-    overlay.classList.toggle("show")
-
     // when modal is shown, page body is fixed
     document.body.style.position = "fixed";
     document.body.style.top = `-${window.scrollY}px`;
 
 
-    entireCover.style.opacity = 0;
+    entireCover.style.opacity = -3;
+    overlay.style.opacity = -3;
     entireCover.style.display = "flex";
+    overlay.style.display = "flex";
     (function fade() {
         let val = parseFloat(entireCover.style.opacity);
         if (!((val += .1) > 1)) {
             entireCover.style.opacity = val;
+            overlay.style.opacity = val;
             requestAnimationFrame(fade);
         }
     })();
 };
 
 const closeModal = function () {
-    overlay.classList.toggle("show")
-
-    // when modal is shown, page body is fixed
+    // when modal is shown, page body is closed
     document.body.style.position = "relative";
 
-    entireCover.style.opacity = 1;
+    entireCover.style.opacity = 4;
+    overlay.style.opacity = 1;
     (function fade() {
-        if ((entireCover.style.opacity -= .1) < 0) {
+        if ((entireCover.style.opacity -= .1) < 0 && (overlay.style.opacity -= .1) < 0) {
             entireCover.style.display = "none";
+            overlay.style.display = "none";
         } else {
             requestAnimationFrame(fade);
         }
